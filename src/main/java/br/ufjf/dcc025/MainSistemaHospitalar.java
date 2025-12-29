7f2db136b229dc59c62d3faa9ae54f76a819b74c
João Vitor Almeida <joaovitoraq2006@gmail.com>
2025-12-29 18:33:21 -0300

    Implementação completa de classe de Verificações de email, senha e cpf e testes na Main

--- src/main/java/br/ufjf/dcc025/MainSistemaHospitalar.java
+++ src/main/java/br/ufjf/dcc025/MainSistemaHospitalar.java
@@ -1,5 +1,12 @@
 package br.ufjf.dcc025;
 
+import br.ufjf.dcc025.model.HorarioAtendimento;
+import br.ufjf.dcc025.model.Medico;
+import br.ufjf.dcc025.model.Usuario;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,28 @@ public class MainSistemaHospitalar
 {
     public static void main( String[] args )
     {
-        System.out.println( "Sistema Iniciado" );
+        String c = "Carlos";
+        String email = "CarlosAlmeida@gmail.com";
+        String senha = "A06k28@";
+        String cpf = "11249175607";
+        Medico medico = new Medico(c, email, senha, cpf);
+
+        System.out.println("Nome do Medico: " +  medico.getNome());
+        System.out.println("CPF do Medico: " +  medico.getCpf());
+        System.out.println("Email do Medico: " +  medico.getEmail());
+
+        medico.setHorariosDisponiveis(DayOfWeek.MONDAY, LocalTime.of(14, 0), LocalTime.of(18, 0), 30 );
+        medico.setHorariosDisponiveis(DayOfWeek.FRIDAY, LocalTime.of(10, 0), LocalTime.of(15, 0), 1 );
+
+        System.out.println("Horarios de Trabalho do medico " + medico.getNome());
+        for( HorarioAtendimento h :  medico.getHorariosDisponiveis())
+            {
+                DayOfWeek dia = h.getDia();
+            LocalTime horaInicio = h.getInicio();
+            LocalTime horaFim = h.getFim();
+            int duracaoAtendimento =  h.getDuracaoAtendimento();
+            System.out.println("Dia: " + dia + " Horario de Incio:  " + horaInicio + " Horario de Fim: " + horaFim +  " Duração de cada Atendimento: " + duracaoAtendimento);
+            }
+
     }
 }
--- src/main/java/br/ufjf/dcc025/util/ValidaDados.java
+++ src/main/java/br/ufjf/dcc025/util/ValidaDados.java
@@ -1,17 +1,88 @@
 package br.ufjf.dcc025.util;
 
+import java.util.InputMismatchException;
+import java.util.regex.Pattern;
+
 public class ValidaDados {
 
     public static boolean  ValidaCPF(String cpf) {
         //validação cpf
-        return true;
+        if ((cpf.length() != 11) ||
+                cpf.equals("00000000000") ||
+                cpf.equals("11111111111") ||
+                cpf.equals("22222222222") || cpf.equals("33333333333") ||
+                cpf.equals("44444444444") || cpf.equals("55555555555") ||
+                cpf.equals("66666666666") || cpf.equals("77777777777") ||
+                cpf.equals("88888888888") || cpf.equals("99999999999"))
+            return false;
+
+        char digito10, digito11;
+        int soma, i, resto, num, peso;
+
+        try {
+            soma = 0;
+            peso = 10;
+            for(i = 0; i < 9; i++)
+            {
+                num = (int)(cpf.charAt(i) - 48);
+                soma += (num * peso);
+                peso = peso - 1;
+            }
+            resto = 11 - (soma % 11);
+            if(resto == 10 || resto == 11)
+                digito10 = '0';
+            else
+                digito10 = (char)(resto + 48);
+
+            soma = 0;
+            peso = 11;
+            for(i = 0; i < 10; i++){
+                num = (int)(cpf.charAt(i) - 48);
+                soma += (num * peso);
+                peso = peso - 1;
+            }
+
+            resto = 11 - (soma % 11);
+            if(resto == 10 || resto == 11)
+                digito11 = '0';
+            else
+                digito11 = (char)(resto + 48);
+
+            if(digito10 == cpf.charAt(9) &&  digito11 == cpf.charAt(10))
+                return true;
+            else
+                return false;
+        }
+        catch(InputMismatchException erro) {
+            return false;
+        }
     }
     public static boolean validaEmail(String email) {
         //regex
-        return true;
+        if(email == null || email.isEmpty())
+            return false;
+
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return Pattern.matches(regex, email);
     }
     public static boolean validaSenha(String senha) {
-        return true;
-        //estipular um tamanho para a senha
+        if (senha.length() < 6) return false;
+
+        boolean achouNumero = false;
+        boolean achouMaiuscula = false;
+        boolean achouMinuscula = false;
+        boolean achouSimbolo = false;
+        for (char c : senha.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                achouNumero = true;
+            } else if (c >= 'A' && c <= 'Z') {
+                achouMaiuscula = true;
+            } else if (c >= 'a' && c <= 'z') {
+                achouMinuscula = true;
+            } else {
+                achouSimbolo = true;
+            }
+        }
+        return achouNumero && achouMaiuscula && achouMinuscula && achouSimbolo;
     }
 }
