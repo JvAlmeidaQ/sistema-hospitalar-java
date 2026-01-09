a495ed2fc3dfa67fc35fc8a92846962a802c535f
Gustavo Bersan Moreira Campos <bersan.gustavo@estudante.ufjf.br>
2026-01-09 18:49:36 -0300

    Add class Autenticar e método ValidacaoSetSenha, lógica de edição da secretária saiu da view e foi para controller

---
+++ src/main/java/br/ufjf/dcc025/controller/AutenticarLogin.java
@@ -0,0 +1,40 @@
+//Fechar login e abrir página principal
+package br.ufjf.dcc025.controller;
+
+import br.ufjf.dcc025.model.*;
+import br.ufjf.dcc025.view.TelaLogin;
+
+import javax.swing.*;
+
+public class AutenticarLogin {
+    private TelaLogin telaLogin;
+
+    public AutenticarLogin(TelaLogin telaLogin){
+        this.telaLogin = telaLogin;
+    }
+
+    public void autenticarUser(String email, String senha){
+        for (Medico m : DadosHospital.medicos){
+            if(m.getEmail().equals(email) && m.getSenha().equals(senha)){
+                JOptionPane.showMessageDialog(telaLogin, "Bem vindo Dr. " + m.getNome());
+                //abrir tela médico e fechar login
+                return;
+            }
+        }
+        for(Paciente p : DadosHospital.pacientes){
+            if(p.getEmail().equals(email) && p.getSenha().equals(senha)){
+                JOptionPane.showMessageDialog(telaLogin, "Bem vindo " + p.getNome());
+                //abrir tela paciente e fechar login
+                return;
+            }
+        }
+        for(Secretaria s : DadosHospital.secretarias){
+            if(s.getEmail().equals(email) && s.getSenha().equals(senha)){
+                JOptionPane.showMessageDialog(telaLogin, "Bem vindo " + s.getNome());
+                //abrir tela secretaria e fechar login
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(telaLogin, "Usuário ou senha inválidos!", "Erro", JOptionPane.ERROR_MESSAGE);
+    }
+}
